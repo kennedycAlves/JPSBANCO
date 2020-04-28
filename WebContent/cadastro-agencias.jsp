437e0c7be53f36c14000556dea07df0bb371e4c9
kennedy <kennedy@localhost>
2020-04-28 08:46:36 -0300

    Cadastro de novas agências criado

---
+++ WebContent/cadastro-agencias.jsp
@@ -0,0 +1,86 @@
+<%@page import="model.dao.DaoFactory"%>
+<%@page import="java.sql.ResultSet"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="model.entities.Agencia" %>
+<%@ page import="model.dao.AgenciaDAO" %>
+<%@ page import="model.dao.impl.AgenciaDAOJDBC" %>
+<%@ page import="db.DB"%>
+<%@ page import="model.entities.Funcionario" %>
+<%@ page import="model.dao.FuncionarioDAO" %>
+<%@ page import="model.dao.impl.FuncionarioDAOJDBC" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form   name="formCadastro" action="inserirAgencia" method="post">
+
+<h2  style="text-align: center;"> Cadastro</h2>
+<table aling='center' border='1'>
+<tr>
+<td><p>Códido da Agencia</p></td><td><SELECT name='cod'>
+<% 
+ResultSet RsResultado2;
+Integer cod = 0, novaAgencia;
+try{
+	AgenciaDAO slcAgencia = DaoFactory.creteAgenciaDAO();
+	
+	Agencia agencia = new Agencia();
+	
+	RsResultado2 = slcAgencia.findAll();
+	
+	out.println("<option> Selecione...</option>");
+	
+	while(RsResultado2.next()){
+		cod = RsResultado2.getInt("COD");
+	
+	}
+	novaAgencia = cod + 1;
+	out.println("<option value="+novaAgencia+">"+novaAgencia+"</option>");
+
+	out.println("</td></SELECT>");
+
+}catch (Exception e) {
+	out.println(e);	
+	}	
+
+%>
+</tr>
+<tr>
+<td><p>Endereco</p></td><td><input name="endereco" size="20" maxlength="20"  type="text"/></td>
+</tr>
+<tr>
+<td><p>Gerente</p></td><td><SELECT name='gerente'>
+<%
+ResultSet rsResultado;
+
+try{ 
+	FuncionarioDAO newfunc = DaoFactory.creteFuncionarioDAO();
+	Funcionario func = new Funcionario();
+	
+	rsResultado = newfunc.findAll();
+	
+	
+
+	out.println("<option> Selecione...</option>");
+	while(rsResultado.next()){
+	
+		out.println("<option value="+rsResultado.getString("MATRICULA")+">"+"Matricula: "+rsResultado.getString("MATRICULA")+ " Gerente:"+rsResultado.getString("NOME")+"</option>");
+	}
+	out.println("</td></SELECT>");
+}catch (Exception e) {
+	out.println(e);	
+	}	
+
+%>
+</tr>
+</table>
+<br>
+<input name="btnConfirmar" value="Confirmar" type="submit">
+</form>
+</body>
+</html>
---
+++ src/model/Servlet/inserirAgencia.java
@@ -0,0 +1,90 @@
+package model.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.entities.Agencia;
+import model.dao.AgenciaDAO;
+import model.dao.DaoFactory;
+import model.dao.impl.AgenciaDAOJDBC;
+
+/**
+ * Servlet implementation class inserirAgencia
+ */
+@WebServlet("/inserirAgencia")
+public class inserirAgencia extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public inserirAgencia() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Integer cod, gerente;
+		String endereco;
+		Integer qtdContas = 0;
+		PrintWriter out;
+		RequestDispatcher rd = null;
+		
+		cod = Integer.parseInt(request.getParameter("cod"));
+		gerente = Integer.parseInt(request.getParameter("gerente"));
+		endereco = request.getParameter("endereco");
+		
+		response.setContentType("text/html;charset=UTF-8");
+		out = response.getWriter();
+    	out.println("<!DocType html>");
+    	out.println("<html>");
+    	out.println("<head>");
+    	out.println("<title>Calculo da área de uma circuferência</title>");
+    	out.println("</head>");
+    	out.println("<body>");
+		
+		try{
+			Agencia agn = new Agencia(endereco, cod, gerente, qtdContas);
+		
+		
+			AgenciaDAO newagn = DaoFactory.creteAgenciaDAO();
+			
+			newagn.insert(agn);
+			
+			out.println("<h3>Agencia cadastrada com sucesso</h3>");	
+			out.println("<br><p>Cadastre novas agências se desejar..</p>");
+			
+			rd = request.getRequestDispatcher("/cadastro-agencias.jsp");
+			//rd.forward(request, response);
+			rd.include(request,response);
+			
+		}catch (Exception e) {
+				out.println(e);
+		}	
+		
+		out.println("</body>");
+		out.println("</html>");
+		
+		}
+		
+}
+
+
